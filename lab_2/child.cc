19e17c3fe3f928daad33c73e7b4cd12aef5577f7
AugustynJ <92784407+AugustynJ@users.noreply.github.com>
2022-11-03 12:04:40 +0100

    Child

---
+++ lab_2/child.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <unistd.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    // int pid;
+    cout << getpid();
+    getchar();
+    
+    return 0;
+}
